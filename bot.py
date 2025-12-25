dddb3a82d795204c260026cb268800d1ec136dbd
SoSalYa <k7790071@gmail.com>
2025-12-26 00:48:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1298,7 +1298,7 @@ class DTEKChecker:
         await self._human_type(city_input, 'княж')
         await asyncio.sleep(2)
         
-        city_option = self.page.locator('#cityautocomplete-list > div:nth-child(2)')
+        city_option = self.page.locator('#cityautocomplete-list > div:nth-child(1)')
         await city_option.wait_for(state='visible', timeout=15000)
         await self._human_move_and_click(city_option)
         await asyncio.sleep(2)
@@ -1313,7 +1313,7 @@ class DTEKChecker:
         await self._human_type(street_input, 'київ')
         await asyncio.sleep(2)
         
-        street_option = self.page.locator('#streetautocomplete-list > div:nth-child(1)')
+        street_option = self.page.locator('#streetautocomplete-list > div:nth-child(2)')
         await street_option.wait_for(state='visible', timeout=15000)
         await self._human_move_and_click(street_option)
         await asyncio.sleep(2)
